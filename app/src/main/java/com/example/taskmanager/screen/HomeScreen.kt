4401c5f0c15840f85ab9af7ba5941eaf71633b62
Piotr <sienkiew@live.com>
2022-04-18 18:02:56 +0200

    no need for a separate top bar composable

--- app/src/main/java/com/example/taskmanager/screen/HomeScreen.kt
+++ app/src/main/java/com/example/taskmanager/screen/HomeScreen.kt
@@ -1,27 +1,26 @@
 package com.example.taskmanager.screen
 
 import TaskList
-import androidx.compose.material.FloatingActionButton
-import androidx.compose.material.Icon
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.example.taskmanager.TopBar
 
 @Composable
 fun HomeScreen(navController: NavController) {
-    Scaffold(topBar = { TopBar() }, floatingActionButton = {
-        FloatingActionButton(
-            onClick = {
-                navController.navigate(route = Screen.CreateTask.route)
-            },
-            content = {
-                Icon(Icons.Default.Add, null)
-            }
-        )
-    }) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "Task Manager") }) },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    navController.navigate(route = Screen.CreateTask.route)
+                },
+                content = {
+                    Icon(Icons.Default.Add, null)
+                }
+            )
+        }) {
         TaskList()
     }
 }
