effd96e09da5f321ac2e2fb6a5ba9c1eca3e32be
Piotr <sienkiew@live.com>
2022-04-18 17:11:56 +0200

    I think it should be like that

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -4,14 +4,27 @@ import TaskList
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import com.example.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Scaffold(topBar = { TopBar() }) {
-                TaskList()
+            TaskManagerTheme {
+                Scaffold(topBar = { TopBar() }, floatingActionButton = {
+                    FloatingActionButton(
+                        onClick = {}, content = {
+                            Icon(Icons.Default.Add, null)
+                        }
+                    )
+                }) {
+                    TaskList()
+                }
             }
         }
     }
