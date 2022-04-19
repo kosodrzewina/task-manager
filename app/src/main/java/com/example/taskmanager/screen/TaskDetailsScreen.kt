53185d75c771af84ce4add809934a0b3d18d0055
Piotr <sienkiew@live.com>
2022-04-20 00:03:33 +0200

    new things in TaskDetailsScreen

--- app/src/main/java/com/example/taskmanager/screen/TaskDetailsScreen.kt
+++ app/src/main/java/com/example/taskmanager/screen/TaskDetailsScreen.kt
@@ -1,16 +1,33 @@
 package com.example.taskmanager.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.taskmanager.R
 import com.example.taskmanager.Task
+import com.example.taskmanager.Urgency
 
 @Composable
 fun TaskDetailsScreen(navController: NavController, task: Task) {
+    val urgencyColor = when (task.urgency) {
+        Urgency.LOW -> colorResource(id = R.color.urgency_low)
+        Urgency.MEDIUM -> colorResource(id = R.color.urgency_medium)
+        Urgency.HIGH -> colorResource(id = R.color.urgency_high)
+    }
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -31,5 +48,40 @@ fun TaskDetailsScreen(navController: NavController, task: Task) {
                 }
             )
         }
-    ) {}
+    ) {
+        Column {
+            Row {
+                Text(
+                    text = "Urgency:",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(all = 16.dp)
+                )
+                Text(
+                    text = task.urgency.toString(),
+                    fontSize = 24.sp,
+                    fontStyle = FontStyle.Italic,
+                    color = urgencyColor,
+                    modifier = Modifier.padding(top = 16.dp, end = 16.dp, bottom = 16.dp)
+                )
+            }
+            Text(
+                text = "Description:",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(all = 16.dp)
+            )
+            Text(
+                text = task.description,
+                fontSize = 18.sp,
+                modifier = Modifier.padding(start = 32.dp, end = 16.dp, bottom = 16.dp)
+            )
+            Text(
+                text = "To Do:",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(all = 16.dp)
+            )
+        }
+    }
 }
