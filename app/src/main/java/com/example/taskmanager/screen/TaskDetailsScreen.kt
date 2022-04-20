bb75d3bb6ec4daa810a2e8d9cf8db7b779c968eb
Piotr <sienkiew@live.com>
2022-04-20 12:30:12 +0200

    checklist

--- app/src/main/java/com/example/taskmanager/screen/TaskDetailsScreen.kt
+++ app/src/main/java/com/example/taskmanager/screen/TaskDetailsScreen.kt
@@ -3,6 +3,8 @@ package com.example.taskmanager.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -12,8 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -29,6 +33,8 @@ fun TaskDetailsScreen(navController: NavController, task: Task) {
         Urgency.MEDIUM -> colorResource(id = R.color.urgency_medium)
         Urgency.HIGH -> colorResource(id = R.color.urgency_high)
     }
+
+    val scrollState = rememberScrollState()
 
     Scaffold(
         topBar = {
@@ -57,7 +63,7 @@ fun TaskDetailsScreen(navController: NavController, task: Task) {
             )
         }
     ) {
-        Column {
+        Column(modifier = Modifier.verticalScroll(scrollState)) {
             Row {
                 Text(
                     text = "Urgency:",
@@ -90,6 +96,24 @@ fun TaskDetailsScreen(navController: NavController, task: Task) {
                 fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(all = 16.dp)
             )
+            task.subtasks.forEach {
+                Row {
+                    Checkbox(
+                        checked = it.second,
+                        onCheckedChange = {},
+                        modifier = Modifier.padding(start = 32.dp, end = 16.dp, bottom = 16.dp)
+                    )
+                    Text(
+                        text = it.first,
+                        fontSize = 18.sp,
+                        style = if (it.second)
+                            TextStyle(textDecoration = TextDecoration.LineThrough)
+                        else
+                            TextStyle.Default,
+                        modifier = Modifier.padding(end = 16.dp, bottom = 16.dp)
+                    )
+                }
+            }
         }
     }
 }
